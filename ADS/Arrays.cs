a9dc0ba5dd67c056110aef7c06e728631a0bc631
muhammet-topcu <muhammet.topcu@hotmail.com>
2024-06-26 10:31:18 +0200

    İnitial commit

---
+++ ADS/Arrays.cs
@@ -0,0 +1,53 @@
+﻿namespace ADS;
+
+public class Arrays
+{
+    public static int RemoveDuplicates(int[] nums)
+    {
+        int count = 0;
+        int current = nums[0];
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] <= current)
+            {
+            }
+            else
+            {
+                current = nums[i];
+                count++;
+                nums[count] = nums[i];
+            }
+        }
+
+        return count + 1;
+    }
+
+    public static int RemoveElement(int[] nums, int val)
+    {
+        int count = 0;
+        for (int i = 0; i < nums.Length;)
+        {
+            if (nums[i] == val)
+            {
+                for (int k = i + 1; k < nums.Length; k++)
+                {
+                    if (k == nums.Length - 1)
+                    {
+                        nums[k - 1] = nums[k];
+                        nums[^1] = 0;
+                        i--;
+                    }
+                    else
+                    {
+                        nums[k - 1] = nums[k];
+                    }
+                }
+
+                count++;
+            }
+
+            i++;
+        }
+        return nums.Length-count;
+    }
+}
---
+++ ADS/Program.cs
@@ -0,0 +1,3 @@
+﻿using ADS;
+
+Arrays.RemoveElement([0,1,2,2,3,0,4,2], 2);
